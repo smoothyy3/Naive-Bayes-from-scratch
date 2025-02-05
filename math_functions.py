d26de1541261ea27f78bfcec278699af74b230a4
jonas <smoothyy.3@gmail.com>
2025-02-05 17:30:06 +0100

    add funcitons for categorical data types

--- math_functions.py
+++ math_functions.py
@@ -84,3 +84,50 @@ def gaussian_PDF(x: float, mean_train_x: float, std_train_x: float) -> float:
     norm_factor = 1 / ((2 * estimate_pi(10000) * (std_train_x ** 2)) ** 0.5)
 
     return norm_factor * (E_APPROX ** exp)
+
+''' 
+CATEGORICAL FEATURE CALCULATIONS
+
+- Conditional probability
+- laplace smoothing
+'''
+
+def conditional_prob(X_train, y_train, feature_index, value, target_class) -> float:
+    # count how often the target class is in the class labels
+    target_instances = 0
+    for x in y_train:
+        if x == target_class:
+            target_instances += 1
+
+    # count how often the feature value appears in the target class
+    feature_instances = 0
+    for i in range(len(y_train)):
+        if y_train[i] == target_class and X_train[i][feature_index] == value:
+            feature_instances += 1
+    
+    # avoid division by 0
+    if target_instances == 0:
+        return 0.0
+    
+    conditional_prob = (feature_instances) / (target_instances)
+    return conditional_prob
+
+def conditional_prob_laplace(X_train, y_train, feature_index, value, target_class, K) -> float:
+    # count how often the target class is in the class labels
+    target_instances = 0
+    for x in y_train:
+        if x == target_class:
+            target_instances += 1
+
+    # count how often the feature value appears in the target class
+    feature_instances = 0
+    for i in range(len(y_train)):
+        if y_train[i] == target_class and X_train[i][feature_index] == value:
+            feature_instances += 1
+    
+    # avoid division by 0
+    if target_instances == 0:
+        return 0.0
+    
+    conditional_prob = (feature_instances + 1) / (target_instances + K)
+    return conditional_prob
